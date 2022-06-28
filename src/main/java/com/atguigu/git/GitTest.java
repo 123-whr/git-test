5937ccbaa39cdcbcf28ab76b8517b9d35eb4662f
whr <whr@atguigu.com>
2022-06-28 12:19:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author whr
+ * @Description
+ * @create 2022-06-28 12:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
